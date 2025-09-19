4071a10b21503331cd222932963234b00a9bb847
NorbertLoh <lohnorbert@gmail.com>
2025-09-19 13:22:38 +0800

    Fix lint and refactor select options to a global constant file

---
+++ frontend/peerprep/app/constants/constants.tsx
@@ -0,0 +1,16 @@
+export const TOPICS = [
+  "Array",
+  "String",
+  "Dynamic Programming",
+  "Backtracking",
+  "Greedy",
+  "Graph",
+  "Tree",
+  "Linked List",
+  "Sorting",
+  "Searching",
+];
+
+export const DIFFICULTIES = ["Easy", "Medium", "Hard"];
+
+export const LANGUAGES = ["C++", "Python", "Java", "JavaScript"];
