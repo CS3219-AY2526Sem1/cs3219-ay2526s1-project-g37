97ea437ee71228c9fa6004fa958ccf4288c5c5e8
NorbertLoh <lohnorbert@gmail.com>
2025-10-03 19:15:00 +0800

    Add collab page

---
+++ frontend/peerprep/app/pages/collabpage.tsx
@@ -0,0 +1,55 @@
+import { Grid, Card, Text } from "@mantine/core";
+import { COLLABCARDHEIGHT } from "~/constants/constants";
+import SessionControlBar from "../components/sessioncontrolbar/SessionControlBar";
+
+import CodeEditor from "../components/codeeditor/CodeEditor";
+
+export default function CollabPage() {
+  return (
+    <>
+      <Grid>
+        <Grid.Col span={{ base: 12 }} align="center">
+          <SessionControlBar />
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 6 }}>
+          <Card style={{ height: COLLABCARDHEIGHT, overflowY: "auto" }} c={"white"}>
+            <Text>Left Side</Text>
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 6 }} style={{ height: COLLABCARDHEIGHT }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: COLLABCARDHEIGHT,
+              gap: "16px",
+            }}
+          >
+            <Card
+              style={{ flexGrow: 1, height: "60%", overflow: "hidden" }}
+              c={"white"}
+            >
+              <CodeEditor
+                defaultLanguage="python"
+                theme="vs-dark"
+                width="100%"
+                height="100%"
+              />
+            </Card>
+
+            <Card
+              style={{
+                flexGrow: 1,
+                height: "calc(40% - 16px)",
+                overflowY: "auto",
+              }}
+              c={"white"}
+            >
+              <Text>Output / Console</Text>
+            </Card>
+          </div>
+        </Grid.Col>
+      </Grid>
+    </>
+  );
+}
--- frontend/peerprep/app/routes.ts
+++ frontend/peerprep/app/routes.ts
@@ -5,4 +5,6 @@ export default [
     route("login", "pages/login.tsx"),
     route("signup", "pages/signup.tsx"),
     route("user", "pages/userpage.tsx"),
+    route("admin/add", "pages/addquestionpage.tsx"),
+    route("collab", "pages/collabpage.tsx"),
 ] satisfies RouteConfig;
