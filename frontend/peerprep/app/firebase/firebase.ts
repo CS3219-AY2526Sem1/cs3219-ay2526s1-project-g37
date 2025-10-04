33c604c747725f14c5c40f6a9582f63b90454af8
davidfoo07 <beckhamfth@gmail.com>
2025-10-04 15:07:43 +0800

    removed firebaseConfig from fe

--- frontend/peerprep/app/firebase/firebase.ts
+++ frontend/peerprep/app/firebase/firebase.ts
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAc-BIyy8f5MyLXCjS67mEJwsbcJTvk6JM",
-    authDomain: "peerprep-4fa3c.firebaseapp.com",
-    projectId: "peerprep-4fa3c",
-    storageBucket: "peerprep-4fa3c.firebasestorage.app",
-    messagingSenderId: "644272889551",
-    appId: "1:644272889551:web:2da99465d93ed7e25ba1ba",
-    measurementId: "G-MH98GY6T4W",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
