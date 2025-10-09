cc71ea707fd6ef3e84b2a4ca952c9ad1ee904356
Billy Ho Cheng En <billyhoce@gmail.com>
2025-10-10 05:23:41 +0800

    Explicitly specify AWS Region in s3 calls

--- services/question-service/app/core/utils.py
+++ services/question-service/app/core/utils.py
@@ -23,6 +23,7 @@ def upload_to_s3(files_and_keys: List[Tuple[bytes, str]]):
         "s3",
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION"),
     )
     bucket_name = os.getenv("S3_BUCKET_NAME")
     for file, key in files_and_keys:
@@ -65,6 +66,7 @@ def delete_from_s3(keys: List[str]):
         "s3",
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION")
     )
     bucket_name = os.getenv("S3_BUCKET_NAME")
     objects_to_delete = [{"Key": key} for key in keys]
