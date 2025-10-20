ddc79f0ebde1c54a16a42bbf4ebaa9cf7b5fab84
kokerinks <limkokliang01@gmail.com>
2025-10-20 23:11:13 +0800

    refactor(fe): use mantine props for styling

--- frontend/peerprep/app/components/collabmodals/endsessionmodal.tsx
+++ frontend/peerprep/app/components/collabmodals/endsessionmodal.tsx
@@ -1,30 +1,37 @@
-import { Modal, Button, Text } from "@mantine/core";
+import { Modal, Button, Text, Group } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-
 
 export default function EndSessionModal({ onEndSession }: { onEndSession: () => void }) {
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <>
-        <Modal opened={opened} onClose={close} centered title="End Session">
-        <Text style={{ marginTop: '10px' }}>
-            Are you sure you want to end this session? This action cannot be undone.
+      <Modal opened={opened} onClose={close} centered>
+        <Text size="xl" fw={700}>
+          End Session
         </Text>
-        <div style={{ display: 'flex', justifyContent: 'flex-end', gap: '10px', marginTop: '20px' }}>
-            <Button color="red" onClick={() => {
-              onEndSession();
+        <Text mt={10}>
+          Are you sure you want to end this session? This action cannot be undone.
+        </Text>
+        <Group justify="flex-end" gap={10} mt={20}>
+          <Button
+            color="red"
+            onClick={() => {
+              if (onEndSession) {
+                onEndSession();
+              }
               close();
-            }}>
+            }}
+          >
             End Session
-            </Button>
-            <Button onClick={close}>
+          </Button>
+          <Button onClick={close}>
             Cancel
-            </Button>
-        </div>
-        </Modal>
-        <Button color="red" onClick={open}>End Session</Button>
+          </Button>
+        </Group>
+      </Modal>
+      <Button color="red" onClick={open}>End Session</Button>
     </>
   );
-};
+}
 
