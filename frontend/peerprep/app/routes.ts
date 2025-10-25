b0213843b3f9903c8a0ba8e8dac9b13e468b30da
NorbertLoh <lohnorbert@gmail.com>
2025-10-25 17:02:44 +0800

    Fix routes

--- frontend/peerprep/app/routes.ts
+++ frontend/peerprep/app/routes.ts
@@ -1,10 +1,10 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
-    index("pages/LoginPage.tsx"),
-    route("signup", "pages/SignupPage.tsx"),
-    route("user", "pages/UserPage.tsx"),
-    route("questions", "pages/QuestionsPage.tsx"),
-    route("questions/add", "pages/AddQuestionPage.tsx"),
-    route("collab/:sessionId", "pages/CollabPage.tsx"),
+    index("Pages/LoginPage.tsx"),
+    route("signup", "Pages/SignupPage.tsx"),
+    route("user", "Pages/UserPage.tsx"),
+    route("questions", "Pages/QuestionsPage.tsx"),
+    route("questions/add", "Pages/AddQuestionPage.tsx"),
+    route("collab/:sessionId", "Pages/CollabPage.tsx"),
 ] satisfies RouteConfig;
