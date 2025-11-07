f6cfd39be1939875f2035bc4e8b1fbb871929351
kokerinks <limkokliang01@gmail.com>
2025-11-07 21:07:37 +0800

    chore(questions): move cors to env

--- services/question-service/app/main.py
+++ services/question-service/app/main.py
@@ -1,15 +1,17 @@
+import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.routers import questions_router, labels_router, images_router
 from fastapi.middleware.cors import CORSMiddleware  
 
+DEFAULT_CORS_ORIGINS = ["http://localhost:5173"]
+
 load_dotenv()
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-]
+origins = os.getenv("QUESTION_CORS_ORIGINS", DEFAULT_CORS_ORIGINS).split(",")
+print(f"Allowed CORS origins:  {origins}") 
 
 app.add_middleware(
     CORSMiddleware,
